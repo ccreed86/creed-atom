611ba370385302fe0b5219085031fb582ef25acb
Clifton <cliftonreed1986@gmail.com>
2024-05-27 13:48:13 -0700

    footer, header done. started woring on nav bar.

--- 00-my-react-app/src/componets/ContactMe/index.jsx
+++
@@ -1,5 +0,0 @@
-// WHEN I am presented with the Contact section
-// THEN I see a contact form with fields for a name, an email address, and a message
-
-// WHEN I enter text into the email address field
-// THEN I receive a notification if I have entered an invalid email address
--- 00-my-react-app/src/componets/Footer/index.jsx
+++ 00-my-react-app/src/componets/Footer/index.jsx
@@ -1,2 +1,36 @@
 // WHEN I view the footer
 // THEN I am presented with text or icon links to the developer’s GitHub and LinkedIn profiles, and their profile on a third platform (Stack Overflow, Twitter) 
+import React from "react";
+import "./style.css";
+
+function Footer() {
+    const icons =  [
+        {
+            name: "fa-brands fa-square-github",
+            links: "https://github.com/ccreed86"
+        },
+        {
+            name: "fa-brands fa-linkedin",
+            links: "https://www.linkedin.com/in/clifton-reed-53844619b/"
+        },
+        {
+            name: "fa-brands fa-stack-overflow",
+            links: "https://stackoverflow.com/users/24299862/cliff"
+        }
+    ]
+
+
+    return (
+        <footer className="footer-style">
+            <section>
+                <span className = "span-style">©2024</span>
+            {icons.map(icon => { 
+                return <a href={icon.links} key={icon.name} target="_blank" rel="noopener no referrer" className="link-style"><i className={icon.name}></i>
+                </a>})}
+            </section>
+        </footer>
+    )
+}
+
+
+export default Footer;
