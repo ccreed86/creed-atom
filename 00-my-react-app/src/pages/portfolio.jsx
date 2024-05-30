c05909dbbf58e9ecec84fa486fbdc6b87ea1b0df
Clifton <cliftonreed1986@gmail.com>
2024-05-30 04:01:54 -0700

    contact and resume body have been added. About section updated with pic. Header format adjusted. screamming into the void complete. aaaaaaaaahhhhhhhhhhhhhhahahahahahahah. it late....

--- 00-my-react-app/src/pages/portfolio.jsx
+++ 00-my-react-app/src/pages/portfolio.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 function PortfolioPage() {
-    return <div>Portfolio</div>
+    return <section>
+        <h1>Portfolio</h1>
+        
+        <div>Project 1</div>
+
+        <div>Project 2</div>
+
+
+        <div>Project 3</div>
+
+
+        <div>Project 4</div>
+
+
+        <div>Project 5</div>
+
+
+        <div>Project 6</div>
+
+        </section>
 }
 
 export default PortfolioPage;
